43919eb385904f87136bf878e0d0ed6cbe4052d3
Krist Wongsuphasawat <krist.wongz@gmail.com>
2014-04-19 20:52:55 -0700

    add binding for data and renderer

--- src/angular-vega.js
+++ src/angular-vega.js
@@ -1,18 +1,51 @@
-var ngVega = angular.module('ngVega', []);
+var ngVega = angular.module('ngVega', ['debounce']);
 
-ngVega.directive('vega', function() {
+ngVega.directive('vega', ['debounce', function(debounce) {
   return {
     restrict: 'AE',
     scope: {
-      spec: '='
+      spec: '=',
+      data: '=vegaData',
+      renderer: '=vegaRenderer'
     },
     link: function(scope, element, attrs) {
       var dom = element[0];
-      scope.$watch('spec', function () {
+      var view;
+
+      function parse(){
+        console.log('parse', scope.renderer);
+
         vg.parse.spec(scope.spec, function(chart) {
-          chart({el: dom}).update();
+          view = chart({
+            el: dom,
+            data: scope.data,
+            renderer: scope.renderer || 'canvas'
+          }).update();
         });
+      }
+
+      var debouncedParse = debounce(parse, 100);
+
+      scope.$watch('spec', debouncedParse, true);
+
+      scope.$watch('data', function(data){
+        if(view){
+          console.log('update data');
+          view.data(data).update().render();
+        }
+        else{
+          debouncedParse();
+        }
       }, true);
+
+      scope.$watch('renderer', function(renderer){
+        if(view){
+          view.renderer(renderer).update();
+        }
+        else{
+          debouncedParse();
+        }
+      });
     }
   };
-});
+}]);
