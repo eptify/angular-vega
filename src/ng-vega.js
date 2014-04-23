24cfc3b717cf87ec6c414adcf7036fcf8049384e
Krist Wongsuphasawat <kristw@twitter.com>
2014-04-22 18:02:27 -0700

    use UMD pattern

--- src/ng-vega.js
+++ src/ng-vega.js
@@ -1,51 +1,78 @@
-var ngVega = angular.module('ngVega', ['debounce']);
+// Define module using Universal Module Definition pattern
+// https://github.com/umdjs/umd/blob/master/amdWeb.js
 
-ngVega.directive('vega', ['debounce', function(debounce) {
-  return {
-    restrict: 'AE',
-    scope: {
-      spec: '=',
-      data: '=vegaData',
-      renderer: '=vegaRenderer'
-    },
-    link: function(scope, element, attrs) {
-      var dom = element[0];
-      var view;
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // Support AMD. Register as an anonymous module.
+    // EDIT: List all dependencies in AMD style
+    define(['angular', 'vega'], factory);
+  } else {
+    // No AMD. Set module as a global variable
+    // EDIT: Pass dependencies to factory function
+    factory(root.angular, root.vg);
+  }
+}(this,
+//EDIT: The dependencies are passed to this function
+function (angular, vg) {
+  //---------------------------------------------------
+  // BEGIN code for this module
+  //---------------------------------------------------
 
-      function parse(){
-        console.log('parse', scope.renderer);
+  var ngVega = angular.module('ngVega', ['debounce']);
 
-        vg.parse.spec(scope.spec, function(chart) {
-          view = chart({
-            el: dom,
-            data: scope.data,
-            renderer: scope.renderer || 'canvas'
-          }).update();
+  ngVega.directive('vega', ['debounce', function(debounce) {
+    return {
+      restrict: 'AE',
+      scope: {
+        spec: '=',
+        data: '=vegaData',
+        renderer: '=vegaRenderer'
+      },
+      link: function(scope, element, attrs) {
+        var dom = element[0];
+        var view;
+
+        function parse(){
+          vg.parse.spec(scope.spec, function(chart) {
+            view = chart({
+              el: dom,
+              data: scope.data,
+              renderer: scope.renderer || 'canvas'
+            }).update();
+          });
+        }
+
+        var debouncedParse = debounce(parse, 100);
+
+        scope.$watch('spec', debouncedParse, true);
+
+        scope.$watch('data', function(data){
+          if(view){
+            view.data(data).update().render();
+          }
+          else{
+            debouncedParse();
+          }
+        }, true);
+
+        scope.$watch('renderer', function(renderer){
+          if(view){
+            view.renderer(renderer).update();
+          }
+          else{
+            debouncedParse();
+          }
         });
       }
+    };
+  }]);
 
-      var debouncedParse = debounce(parse, 100);
+  return ngVega;
 
-      scope.$watch('spec', debouncedParse, true);
+  //---------------------------------------------------
+  // END code for this module
+  //---------------------------------------------------
+}));
 
-      scope.$watch('data', function(data){
-        if(view){
-          console.log('update data');
-          view.data(data).update().render();
-        }
-        else{
-          debouncedParse();
-        }
-      }, true);
 
-      scope.$watch('renderer', function(renderer){
-        if(view){
-          view.renderer(renderer).update();
-        }
-        else{
-          debouncedParse();
-        }
-      });
-    }
-  };
-}]);
+
